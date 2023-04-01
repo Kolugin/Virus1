f839edb2739ad1d0ba0f914b7f750289b59e8398
Kolugin <nikita_chizh@mail.ru>
2023-04-01 12:38:18 +0300

    Virus in C++
    Virus in C++

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,13 @@
+﻿#include <thread>
+#include <vector>
+
+int main()
+{
+	std::vector<std::thread> threads;
+
+	for (int i = 0; i < 8; i++)
+	{
+		threads.push_back(std::thread([]() {while (true) {}}));
+	}
+	while (true) {}
+}
